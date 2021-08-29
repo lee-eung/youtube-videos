80ab6ebfee05f38c523f4f62bbb420ce23613e52
Mincheol-Kang <facethemind@gmail.com>
2021-08-29 22:04:37 +0900

    최근 영상 갯수 지정해서 가져오기 기능 추가, 입력란에서 엔터쳐도 실행되게

--- src/App.svelte
+++ src/App.svelte
@@ -6,7 +6,7 @@ let playlist_id = ''
 let progress_log = ''
 let result_json = ''
 let how_many_videos = 50;
-let max_videos = -1;
+let need_videos = -1;
 let num_of_videos_class = 'hidden';
 let copy_button_class = 'hidden';
 let api_key_input_msg = '';
@@ -39,6 +39,7 @@ function startIntervalFetch() {
 	if( !isInputOk() )
 		return
 	writeProgressLog('유튜브 채널 영상 목록을 가져오기 시작합니다.\n')
+	writeProgressLog('(아래 출력되는 번호들은 영상의 index 번호이며 0번부터 시작합니다.)\n')
 	writeProgressLog('----------------------------------------\n')
 	interval = setInterval(() => {
 		sendRequest()
@@ -46,6 +47,7 @@ function startIntervalFetch() {
 }
 
 function isInputOk() {
+	initializeAll()
 	if(api_key.trim().length < 5) {
 		api_key_input.focus()
 		api_key_input_msg = 'API Key를 입력해주세요.'
@@ -56,16 +58,6 @@ function isInputOk() {
 		playlist_id_input_msg = 'YouTube Channel List ID를 입력해주세요.'
 		return false
 	}
-	if(checkbox.checked) {
-		max_videos = -1
-		option_params['maxResults'] = DEFAULT_MAX_RESULTS
-	} else {
-		max_videos = how_many_videos
-		if(how_many_videos < DEFAULT_MAX_RESULTS) {
-			option_params['maxResults'] = how_many_videos
-		}
-	}
-	initializeAll()
 	return true
 }
 
@@ -76,13 +68,30 @@ function initializeAll() {
 	copy_button_class = 'hidden';
 	progress_log = ''
 	result_json = ''
+	need_videos = checkbox.checked ? -1 : how_many_videos
 	option_params['pageToken'] = ''
 	option_params['key'] = api_key
 	option_params['playlistId'] = playlist_id
 	playlist['videos'] = []
 }
 
+function getMaxResults() {
+	if(checkbox.checked) {
+		return DEFAULT_MAX_RESULTS
+	}
+	const current_videos = playlist['videos'].length
+	need_videos = how_many_videos - current_videos
+	const max_results = need_videos < DEFAULT_MAX_RESULTS ? need_videos : DEFAULT_MAX_RESULTS
+	IS_DEBUG_MODE && console.log({
+		'current_videos': current_videos,
+		'need_videos': need_videos,
+		'max_results': max_results,
+	})
+	return max_results
+}
+
 function getURL() {
+	option_params['maxResults'] = getMaxResults()
 	let params = []
 	for(let option in option_params) {
 		params.push(`${option}=${option_params[option]}`)
@@ -103,7 +112,7 @@ function getVideoList(body) {
 		result.items.forEach(item => {
 			const video_info = getVideoInfo(item)
 			playlist['videos'].length > 1 && writeProgressLog(', ')
-			writeProgressLog(video_info.position + 1)
+			writeProgressLog(video_info.index)
 		})
 		checkKeepGoing(result)
 	} catch (e) {
@@ -116,8 +125,11 @@ function getVideoList(body) {
 
 function checkKeepGoing(result) {
 	option_params['pageToken'] = result.nextPageToken
-	const total_results = max_videos > 0 ? max_videos : result.pageInfo.totalResults
-	keep_going = (playlist['videos'].length < total_results)
+	if(checkbox.checked) {
+		keep_going = (playlist['videos'].length < result.pageInfo.totalResults)
+	} else {
+		keep_going = (need_videos > DEFAULT_MAX_RESULTS)
+	}
 	IS_DEBUG_MODE && console.log({
 		'number of videos': playlist['videos'].length,
 		'keep_going': keep_going,
@@ -128,14 +140,14 @@ function checkKeepGoing(result) {
 		copy_button_class = '';
 		writeProgressLog('\n----------------------------------------\n')
 		writeProgressLog('유튜브 채널 영상 목록 가져오기를 마쳤습니다.\n')
-		result_json = JSON.stringify(playlist)
+		result_json = JSON.stringify(playlist, null, 4)
 	}
 }
 
 function getVideoInfo(item) {
 	const video_info = {
-		position: item.snippet.position,
-		publishedAt: item.snippet.publishedAt,
+		index: item.snippet.position,
+		publishedAt: item.snippet.publishedAt.replace('T', ' ').replace('Z', ''),
 		videoId: item.snippet.resourceId.videoId,
 		videoURL: `https://www.youtube.com/watch?v=${item.snippet.resourceId.videoId}`,
 		thumbnail: item.snippet.thumbnails.high.url,
@@ -168,6 +180,12 @@ function toggleHowManyVideos() {
 function writeProgressLog(message) {
 	progress_log += message
 }
+
+const onKeyPress = e => {
+    if(e.charCode === 13) {
+		startIntervalFetch()
+	}
+}
 </script>
 
 <svelte:head>
@@ -178,15 +196,30 @@ function writeProgressLog(message) {
 	<header>{PAGE_TITLE}</header>
 	<div class="form">
 		<div>
-			YouTube Data API v3 - API Key: <input type="password" bind:value={api_key} bind:this={api_key_input}> <span class="input_msg">{api_key_input_msg}</span>
+			YouTube Data API v3 - API Key: <input
+				type="password" bind:value={api_key}
+				bind:this={api_key_input}
+				on:keypress={onKeyPress}> <span 
+				class="input_msg">{api_key_input_msg}</span>
 		</div>
 		<div>
-			YouTube Channel List ID: <input type="text" bind:value={playlist_id} bind:this={playlist_id_input}> <span class="input_msg">{playlist_id_input_msg}</span>
+			YouTube Channel List ID: <input
+				type="text" bind:value={playlist_id}
+				bind:this={playlist_id_input}
+				on:keypress={onKeyPress}> <span
+				class="input_msg">{playlist_id_input_msg}</span>
 		</div>
-		<input type="checkbox" bind:this={checkbox} checked on:click={toggleHowManyVideos} class="checkbox_class"> <span class={get_all_text_class}>전체</span>
-		<span class={num_of_videos_class}>최근 <input type="number" bind:value={how_many_videos} min="1" class="input_number">개</span>
+		<input type="checkbox" bind:this={checkbox}
+			checked on:click={toggleHowManyVideos} class="checkbox_class"> <span
+			class={get_all_text_class}>전체</span>
+		<span class={num_of_videos_class}>최근 <input
+			type="number" bind:value={how_many_videos}
+			on:keypress={onKeyPress}
+			min="1" class="input_number">개</span>
 		<button on:click={startIntervalFetch}>영상 목록 가져오기</button>
-		<button on:click={copyResultJson} class={copy_button_class}>json 내용을 클립보드에 복사하기</button> <span class="copy_result_msg">{copy_result_msg}</span>
+		<button on:click={copyResultJson}
+			class="button-primary {copy_button_class}">json 내용을 클립보드에 복사하기</button> <span
+			class="copy_result_msg">{copy_result_msg}</span>
 		<div id="clipboard"></div>
 	</div>
 	<div>
@@ -235,5 +268,9 @@ textarea {
 }
 .copy_result_msg {
 	color: blue;
+}
+.button-primary {
+	background-color: blue;
+	color: white;
 }
 </style>
