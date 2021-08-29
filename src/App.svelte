092bf51967367e0d8a9cc36d91ad20bccde8a475
Mincheol-Kang <facethemind@gmail.com>
2021-08-29 17:22:05 +0900

    변수명은 팟홀표기법으로, 함수명은 카멜표기법으로 일관성있게 변경

--- src/App.svelte
+++ src/App.svelte
@@ -6,25 +6,25 @@ let playlist_id = ''
 let progress_log = ''
 let result_json = ''
 let copy_button = 'hidden';
-let apiKeyInputMsg = '';
-let playlistIdInputMsg = '';
-let copyResultMsg = '';
+let api_key_input_msg = '';
+let playlist_id_input_msg = '';
+let copy_result_msg = '';
 let keep_going = true
-let apiKeyInput
-let playlistIdInput
+let api_key_input
+let playlist_id_input
 let interval
 
 const PAGE_TITLE = '유튜브 채널 동영상 전체목록 가져오기'
 const URL_PREFIX = "https://www.googleapis.com/youtube/v3/playlistItems?"
 const playlist = { videos: [] }
 
-const optionParams = {
+const option_params = {
 	part		: "snippet",
 	playlistId	: "",
 	key			: "",
 	maxResults	: 50
 }
-const fetchOptions = {
+const FETCH_OPTIONS = {
 	method: 'GET',
 }
 
@@ -33,66 +33,66 @@ const copyResultJson = () => {
 		target: document.getElementById('clipboard'),
 		props: { result_json },
 	})
-	copyResultMsg = '클립보드에 복사되었습니다.'
+	copy_result_msg = '클립보드에 복사되었습니다.'
 	app.$destroy()
 }
 
 function startIntervalFetch() {
-	if( !is_input_ok() )
+	if( !isInputOk() )
 		return
-	write_progress_log('유튜브 채널 영상 목록을 가져오기 시작합니다.\n')
-	write_progress_log('----------------------------------------\n')
+	writeProgressLog('유튜브 채널 영상 목록을 가져오기 시작합니다.\n')
+	writeProgressLog('----------------------------------------\n')
 	interval = setInterval(() => {
 		sendRequest()
 	}, 3000)
 }
 
-function is_input_ok() {
+function isInputOk() {
 	if(api_key.trim().length < 5) {
-		apiKeyInput.focus()
-		apiKeyInputMsg = 'API Key를 입력해주세요.'
+		api_key_input.focus()
+		api_key_input_msg = 'API Key를 입력해주세요.'
 		return false
 	}
-	apiKeyInputMsg = ''
+	api_key_input_msg = ''
 	if(playlist_id.trim().length < 5) {
-		playlistIdInput.focus()
-		playlistIdInputMsg = 'YouTube Channel List ID를 입력해주세요.'
+		playlist_id_input.focus()
+		playlist_id_input_msg = 'YouTube Channel List ID를 입력해주세요.'
 		return false
 	}
-	playlistIdInputMsg = ''
-	optionParams['key'] = api_key
-	optionParams['playlistId'] = playlist_id
+	playlist_id_input_msg = ''
+	option_params['key'] = api_key
+	option_params['playlistId'] = playlist_id
 	return true
 }
 
 function getURL() {
 	let params = []
-	for(let option in optionParams) {
-		params.push(`${option}=${optionParams[option]}`)
+	for(let option in option_params) {
+		params.push(`${option}=${option_params[option]}`)
 	}
 	return URL_PREFIX + params.join('&')
 }
 
 function sendRequest() {
-	fetch(getURL(), fetchOptions)
+	fetch(getURL(), FETCH_OPTIONS)
 		.then(response => response.text())
-		.then(result => get_video_list(result))
+		.then(result => getVideoList(result))
 		.catch(error => console.log('error', error))
 }
 
-function get_video_list(body) {
+function getVideoList(body) {
 	const result = JSON.parse(body)
 	const items = result.items
 	for (let index in items) {
-		const video_info = get_video_info(items[index])
-		playlist['videos'].length > 1 && write_progress_log(', ')
-		write_progress_log(video_info.position + 1)
+		const video_info = getVideoInfo(items[index])
+		playlist['videos'].length > 1 && writeProgressLog(', ')
+		writeProgressLog(video_info.position + 1)
 	}
-	check_keep_going(result)
+	checkKeepGoing(result)
 }
 
-function check_keep_going(result) {
-	optionParams['pageToken'] = result.nextPageToken
+function checkKeepGoing(result) {
+	option_params['pageToken'] = result.nextPageToken
 	keep_going = (playlist['videos'].length < result.pageInfo.totalResults)
 	// console.log({
 	// 	'number of videos': playlist['videos'].length,
@@ -102,13 +102,13 @@ function check_keep_going(result) {
 	if (!keep_going) {
 		clearInterval(interval)
 		copy_button = '';
-		write_progress_log('\n----------------------------------------\n')
-		write_progress_log('유튜브 채널 영상 목록 가져오기를 마쳤습니다.\n')
+		writeProgressLog('\n----------------------------------------\n')
+		writeProgressLog('유튜브 채널 영상 목록 가져오기를 마쳤습니다.\n')
 		result_json = JSON.stringify(playlist)
 	}
 }
 
-function get_video_info(item) {
+function getVideoInfo(item) {
 	const video_info = {
 		position: item.snippet.position,
 		publishedAt: item.snippet.publishedAt,
@@ -122,7 +122,7 @@ function get_video_info(item) {
 	return video_info
 }
 
-function write_progress_log(message) {
+function writeProgressLog(message) {
 	progress_log += message
 }
 </script>
@@ -135,13 +135,13 @@ function write_progress_log(message) {
 	<header>{PAGE_TITLE}</header>
 	<div class="form">
 		<div>
-			YouTube Data API v3 - API Key: <input type="password" bind:value={api_key} bind:this={apiKeyInput}> <span class="input_msg">{apiKeyInputMsg}</span>
+			YouTube Data API v3 - API Key: <input type="password" bind:value={api_key} bind:this={api_key_input}> <span class="input_msg">{api_key_input_msg}</span>
 		</div>
 		<div>
-			YouTube Channel List ID: <input type="text" bind:value={playlist_id} bind:this={playlistIdInput}> <span class="input_msg">{playlistIdInputMsg}</span>
+			YouTube Channel List ID: <input type="text" bind:value={playlist_id} bind:this={playlist_id_input}> <span class="input_msg">{playlist_id_input_msg}</span>
 		</div>
 		<button on:click={startIntervalFetch}>영상 리스트 가져오기</button>
-		<button on:click={copyResultJson} class={copy_button}>json 내용을 클립보드에 복사하기</button> <span class="copy_result_msg">{copyResultMsg}</span>
+		<button on:click={copyResultJson} class={copy_button}>json 내용을 클립보드에 복사하기</button> <span class="copy_result_msg">{copy_result_msg}</span>
 		<div id="clipboard"></div>
 	</div>
 	<div>
